02c2d716cd54881f686ed4925f3982a299e02a27
jordan-grindrod <jlgrindrod@hotmail.com>
2019-11-26 08:27:12 +0000

    first commit

---
+++ tests/count_test.py
@@ -0,0 +1,11 @@
+import pytest
+from applications import count
+
+def test_count_zeros():
+	assert count.count([0,0,0],0)==3
+
+def test_count_string():
+	assert count.count(["a","a","a"],"a")==3
+
+def test_count_minus():
+	assert count.count([-1,-3,-5,-4], -1)==1
