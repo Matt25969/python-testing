2b6588ffe34efb2ae591d1c8b4b3f1caf53203aa
Matt25969 <matt.jo.hunt@gmail.com>
2019-11-26 13:29:53 +0000

    Change

--- tests/count_test.py
+++ tests/count_test.py
@@ -2,7 +2,7 @@ import pytest
 from applications import count
 
 def test_count_zeros():
-	assert count.count([0,0,0],0)==3
+	assert count.count([0,0,0],0)==4
 
 def test_count_string():
 	assert count.count(["a","a","a"],"a")==3
